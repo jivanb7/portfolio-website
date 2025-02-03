7a5c425af6ad8f5648e6b8156418f22c97b592c8
jivanb7 <bal.jivan@yahoo.com>
2025-02-02 20:02:30 -0800

    portfolio new website

---
+++ vite-project/app/routes/home.tsx
@@ -0,0 +1,13 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Jivan's Portfolio" },
+    { name: "description", content: "Welcome to Jivan's Website!" },
+  ];
+}
+
+export default function Home() {
+  return <Welcome />;
+}
